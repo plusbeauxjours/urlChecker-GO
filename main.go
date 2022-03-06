0ae9fdd7432f60924e7feee262b5d0c02f768f0e
🫒plusbeauxjours <plusbeauxjours@gmail.com>
2022-03-06 19:57:51 +0700

    set: goroutines + urlChecker

--- main.go
+++ main.go
@@ -4,62 +4,40 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
-	"time"
 )
+
+type result struct {
+	url    string
+	status string
+}
 
 var errRequestFailed = errors.New("Request failed")
 
 func main() {
-	// var results = make(map[string]string)
-	// urls := []string{
-	// 	"https://www.airbnb.com/",
-	// 	"https://www.google.com/",
-	// 	"https://www.amazon.com/",
-	// 	"https://www.google.com/",
-	// 	"https://soundcloud.com/",
-	// 	"https://www.facebook.com/",
-	// 	"https://www.instagram.com/",
-	// 	"https://www.plusbeauxjours.com/",
-	// }
-	// for _, url := range urls {
-	// 	result := "OK"
-	// 	err := hitURL(url)
-	// 	if err != nil {
-	// 		result = "FAILED"
-	// 	}
-	// 	results[url] = result
-	// }
-	// for url, result := range results {
-	// 	fmt.Println(url, result)
-	// }
-	c := make(chan string)
-	people := [4]string{"mj", "dq", "james", "jake"}
-	for _, person := range people {
-		go isCount(person, c)
+	// results := make(map[string]string)
+	c := make(chan result)
+	urls := []string{
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.google.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://www.plusbeauxjours.com/",
 	}
-	for i := 0; i < len(people); i++ {
-		fmt.Println(<-c)
+	for _, url := range urls {
+		go hitURL(url, c)
 	}
+
 }
 
-func hitURL(url string) error {
+func hitURL(url string, c chan<- result) {
 	fmt.Println("Checking:", url)
 	resp, err := http.Get(url)
+	status := "OK"
 	if err != nil || resp.StatusCode >= 400 {
-		fmt.Println(err, resp.StatusCode)
-		return errRequestFailed
+		status = "FAILED"
 	}
-	return nil
-}
-
-func count(person string) {
-	for i := 0; i < 10; i++ {
-		fmt.Println(person, "is sexy", i)
-		time.Sleep(time.Second)
-	}
-}
-
-func isCount(person string, c chan string) {
-	time.Sleep(time.Second * 5)
-	c <- person + " is sexy"
+	c <- result{url: url, status: status}
 }
